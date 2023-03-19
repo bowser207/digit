3836481d31b480d179c6e469864798d126c46386
bowser207 <109121000+bowser207@users.noreply.github.com>
2023-03-19 11:01:33 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+    function setup() {
+        createCanvas(400, 200);
+      }
+      
+      function draw_a() {
+
+        fill(0, 0, 255);
+        stroke(255, 0, 0);
+        circle(255, 100, 75)
+      }
+
+    function draw_b()
+    {
+      fill(0, 0, 255);
+      stroke(0, 255, 0);
+      circle(153, 194, 75)
+    }
+
+    function draw_c()
+    {
+      fill(255, 0, 0);
+      stroke(255, 255, 0)
+    }
